ded802a35793e2816f73e7c8e14cf00c59364458
isurika <153486985+CeeeTech@users.noreply.github.com>
2024-01-21 21:00:51 +0530

    profile update

--- client/src/views/pages/account/profile.js
+++ client/src/views/pages/account/profile.js
@@ -2,13 +2,14 @@ import * as React from 'react';
 import TextField from '@mui/material/TextField';
 import Grid from '@mui/material/Grid';
 import MainCard from 'ui-component/cards/MainCard';
-import { Button, CardActions, Divider, InputAdornment, Typography, useMediaQuery } from '@mui/material';
+import { Button, CardActions, Divider, InputAdornment, useMediaQuery, Typography, Alert } from '@mui/material';
 import { useTheme } from '@mui/material/styles';
-import { AccountCircle } from '@mui/icons-material';
 import LockIcon from '@mui/icons-material/Lock';
+import AccountCircleIcon from '@mui/icons-material/AccountCircle';
 import EmailIcon from '@mui/icons-material/Email';
+import * as Yup from 'yup';
+import { Formik } from 'formik';
 import { useAuthContext } from '../../../context/useAuthContext';
-// import MergeTypeIcon from '@mui/icons-material/MergeType';
 import { useEffect } from 'react';
 import config from '../../../config';
 
@@ -28,133 +29,166 @@ export default function UserForm() {
     }
   }, [user]);
 
-  const handleSubmit = async (e) => {
-    e.preventDefault();
-
-    // Gather form data
-    const formData = {
-      name: e.target.name.value,
-      email: e.target.email.value,
-      password: e.target.password.value,
-      conform_password: e.target.conform_password.value
-    };
-
-    const id = user?._id;
-
+  const handleSubmit = async (values, helpers) => {
     try {
-      // Perform the form submission logic, e.g., send data to the server
+      const id = user?._id;
       const res = await fetch(config.apiUrl + `api/updatePassword/${id}`, {
         method: 'PUT',
         headers: {
           'Content-Type': 'application/json'
         },
-        body: JSON.stringify(formData)
+        body: JSON.stringify(values)
       });
 
-      // Handle the server response if needed
       console.log('Server response:', await res.json());
     } catch (error) {
       console.error('Error submitting form:', error);
+      helpers.setErrors({ submit: error.message });
+      helpers.setSubmitting(false);
     }
   };
 
   return (
     <>
       <MainCard title="Manage My Profile">
-        <form onSubmit={handleSubmit}>
-          <Grid container direction="column" justifyContent="center">
-            <Grid container sx={{ p: 3 }} spacing={matchDownSM ? 0 : 2}>
-              <Grid item xs={12} sm={6}>
-                <Typography variant="h5" component="h5">
-                  Name
-                </Typography>
-                <TextField
-                  fullWidth
-                  margin="normal"
-                  name="name"
-                  type="text"
-                  value={formData.name || ''}
-                  sx={{ ...theme.typography.customInput }}
-                  InputProps={{
-                    startAdornment: (
-                      <InputAdornment position="start">
-                        <AccountCircle />
-                      </InputAdornment>
-                    )
-                  }}
-                />
+        <Formik
+          enableReinitialize
+          initialValues={{
+            name: formData.name || '',
+            email: formData.email || '',
+            password: '',
+            confirm_password: ''
+          }}
+          validationSchema={Yup.object().shape({
+            name: Yup.string().required('Name is required'),
+            password: Yup.string().min(5, 'Password must be at least 5 characters').required('Password is required'),
+            confirm_password: Yup.string()
+              .oneOf([Yup.ref('password'), null], 'Passwords must match')
+              .required('Confirm Password is required')
+          })}
+          onSubmit={handleSubmit}
+        >
+          {({ errors, handleBlur, handleChange, handleSubmit, isSubmitting, touched, values }) => (
+            <form onSubmit={handleSubmit}>
+              <Grid container direction="column" justifyContent="center">
+                <Grid container spacing={matchDownSM ? 3 : 2}>
+                  <Grid item xs={12} sm={6}>
+                    <Typography variant="h5" component="h5">
+                      Name
+                    </Typography>
+                    <TextField
+                      fullWidth
+                      label="Name"
+                      margin="dense"
+                      name="name"
+                      onBlur={(e) => {
+                        handleBlur(e);
+                        handleChange(e);
+                      }}
+                      onChange={handleChange}
+                      defaultValue={formData.name}
+                      value={values.name}
+                      variant="outlined"
+                      error={Boolean(touched.name && errors.name)}
+                      helperText={touched.name && errors.name}
+                      InputProps={{
+                        startAdornment: (
+                          <InputAdornment position="start">
+                            <AccountCircleIcon />
+                          </InputAdornment>
+                        )
+                      }}
+                    />
+                  </Grid>
+                  <Grid item xs={12} sm={6}>
+                    <Typography variant="h5" component="h5">
+                      Email
+                    </Typography>
+                    <TextField
+                      fullWidth
+                      label="Email"
+                      margin="dense"
+                      name="email"
+                      onBlur={handleBlur}
+                      onChange={handleChange}
+                      value={formData.email}
+                      variant="outlined"
+                      disabled
+                      InputProps={{
+                        startAdornment: (
+                          <InputAdornment position="start">
+                            <EmailIcon />
+                          </InputAdornment>
+                        )
+                      }}
+                    />
+                  </Grid>
+                  <Grid item xs={12} sm={6}>
+                    <Typography variant="h5" component="h5">
+                      New Password
+                    </Typography>
+                    <TextField
+                      fullWidth
+                      type="password"
+                      label="New Password"
+                      margin="dense"
+                      name="password"
+                      onBlur={handleBlur}
+                      onChange={handleChange}
+                      value={values.password}
+                      variant="outlined"
+                      error={Boolean(touched.password && errors.password)}
+                      helperText={touched.password && errors.password}
+                      InputProps={{
+                        startAdornment: (
+                          <InputAdornment position="start">
+                            <LockIcon />
+                          </InputAdornment>
+                        )
+                      }}
+                    />
+                  </Grid>
+                  <Grid item xs={12} sm={6}>
+                    <Typography variant="h5" component="h5">
+                      Confirm Password
+                    </Typography>
+                    <TextField
+                      fullWidth
+                      type="password"
+                      label="Confirm Password"
+                      margin="dense"
+                      name="confirm_password"
+                      onBlur={handleBlur}
+                      onChange={handleChange}
+                      value={values.confirm_password}
+                      variant="outlined"
+                      error={Boolean(touched.confirm_password && errors.confirm_password)}
+                      helperText={touched.confirm_password && errors.confirm_password}
+                      InputProps={{
+                        startAdornment: (
+                          <InputAdornment position="start">
+                            <LockIcon />
+                          </InputAdornment>
+                        )
+                      }}
+                    />
+                  </Grid>
+                </Grid>
+                <Divider sx={{ mt: 3, mb: 2 }} />
+                <CardActions sx={{ justifyContent: 'flex-end' }}>
+                  <Button type="submit" variant="contained" color="primary" disabled={isSubmitting}>
+                    Save Changes
+                  </Button>
+                </CardActions>
+                {errors.submit && (
+                  <Alert severity="error" sx={{ mt: 2 }}>
+                    {errors.submit}
+                  </Alert>
+                )}
               </Grid>
-              <Grid item xs={12} sm={6}>
-                <Typography variant="h5" component="h5">
-                  Email
-                </Typography>
-                <TextField
-                  fullWidth
-                  disabled
-                  margin="normal"
-                  name="email"
-                  type="email"
-                  value={formData.email || ''}
-                  sx={{ ...theme.typography.customInput }}
-                  InputProps={{
-                    startAdornment: (
-                      <InputAdornment position="start">
-                        <EmailIcon />
-                      </InputAdornment>
-                    )
-                  }}
-                />
-              </Grid>
-              <Grid item xs={12} sm={6}>
-                <Typography variant="h5" component="h5">
-                  Password
-                </Typography>
-                <TextField
-                  fullWidth
-                  margin="normal"
-                  name="password"
-                  type="password"
-                  defaultValue=""
-                  sx={{ ...theme.typography.customInput }}
-                  InputProps={{
-                    startAdornment: (
-                      <InputAdornment position="start">
-                        <LockIcon />
-                      </InputAdornment>
-                    )
-                  }}
-                />
-              </Grid>
-
-              <Grid item xs={12} sm={6}>
-                <Typography variant="h5" component="h5">
-                  Confirm Password
-                </Typography>
-                <TextField
-                  fullWidth
-                  margin="normal"
-                  name="conform_password"
-                  type="password"
-                  defaultValue=""
-                  sx={{ ...theme.typography.customInput }}
-                  InputProps={{
-                    startAdornment: (
-                      <InputAdornment position="start">
-                        <LockIcon />
-                      </InputAdornment>
-                    )
-                  }}
-                />
-              </Grid>
-              <Divider />
-            </Grid>
-            <CardActions sx={{ justifyContent: 'flex-end' }}>
-              <Button variant="contained" type="submit">
-                Update
-              </Button>
-            </CardActions>
-          </Grid>
-        </form>
+            </form>
+          )}
+        </Formik>
       </MainCard>
     </>
   );
