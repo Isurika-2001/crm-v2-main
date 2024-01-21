eb5534dba07135a326c43492a61c3213d32428d4
isurika <153486985+CeeeTech@users.noreply.github.com>
2024-01-21 21:51:27 +0530

    logo

--- client/src/layout/MainLayout/LogoSection/index.js
+++ client/src/layout/MainLayout/LogoSection/index.js
@@ -6,7 +6,6 @@ import { ButtonBase } from '@mui/material';
 
 // project imports
 import config from 'config';
-import Logo from 'ui-component/Logo';
 import { MENU_OPEN } from 'store/actions';
 
 // ==============================|| MAIN LOGO ||============================== //
@@ -14,9 +13,10 @@ import { MENU_OPEN } from 'store/actions';
 const LogoSection = () => {
   const defaultId = useSelector((state) => state.customization.defaultId);
   const dispatch = useDispatch();
+
   return (
     <ButtonBase disableRipple onClick={() => dispatch({ type: MENU_OPEN, id: defaultId })} component={Link} to={config.defaultPath}>
-      <Logo />
+      <img src="../../../../assets/images/sltc-logo.png" alt="My Logo" style={{ maxWidth: '100%', height: 'auto' }} />
     </ButtonBase>
   );
 };
